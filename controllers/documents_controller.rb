7fc220a3fdc3dd4d2b820c8c835ed25bd7d1ac91
ebaez <ebaez@it10coop.com.ar>
2020-10-18 19:50:23 -0300

    Repare metodo en la clase documents_controller

--- controllers/documents_controller.rb
+++ controllers/documents_controller.rb
@@ -11,14 +11,20 @@ require 'sinatra-websocket'
 class DocumentsController < Sinatra::Base
   set :views, settings.root + '/views'
   set :userlist, []
+
   attr_accessor :users, :documents, :is_admin
-  # Endpoints for upload a document
-  get '/documents' do
-    user = User.find_by_id(session[:user_id])
-    if user.type == 'admin'
-      @is_admin = true
-      @documents = Document.bring_all
-      @users = User.bring_all
+
+  def find_connection(user)
+    App.sockets.each { |s| return s[:socket] if s[:user] == user.id }
+
+    nil # Por si el usuario no esta conectado en ese momento
+  end
+
+  def get_documents(user)
+    if user == 'admin'
+      self.is_admin = true
+      self.documents = Document.all
+      self.users = User.all
       erb :upload, layout: :layoutlogin
     else
       @error = 'Para acceder a documentos debe ser administrador, ' \
@@ -28,6 +34,19 @@ class DocumentsController < Sinatra::Base
   end
 
 
+  # Endpoints for upload a document
+  get '/documents' do
+    get_documents(User.find_by_id(session[:user_id]).type)
+  end
+
+  post '/documents' do
+    filter_docs = Document.all
+
+    doc_date = params[:date] == '' ? filter_docs : Document.first(date: params[:date])
+    filter_docs = params[:date] == '' ? filter_docs : filter_docs.select { |d| d.date == doc_date.date }
+    self.documents = filter_docs
+    erb :upload, layout: :layoutlogin
+  end
   get '/userdocs' do
     self.documents = User.find_by_id(session[:user_id]).documents
     erb :userdocs, layout: :layoutlogin
@@ -41,15 +60,21 @@ class DocumentsController < Sinatra::Base
   end
 
   post '/save_documents' do
-
+    create_file
     user = User.find_by_id(session[:user_id]).username
     doc = Document.new(name: @filename,
                        date: params['date'],
                        uploader: user,
                        subject: params['subject'])
-    ## if doc.save return true
-    if doc.save
 
+    if doc.save
+      ## extraer esta parte de codigo en un nuevo metodo
+      tagged_document(doc)
+      redirect '/documents'
+    else
+      [500, {}, 'Internal Server Error']
+    end
+  end
 
   def create_file()
     @filename = params[:file][:filename]
@@ -68,11 +93,9 @@ class DocumentsController < Sinatra::Base
         sockets_to_be_notified = []
 
         settings.userlist.each do |tagged_user|
-          sockets_to_be_notified << (User.find_connection(tagged_user)) unless User.find_connection(tagged_user).nil?
+          sockets_to_be_notified << (find_connection(tagged_user)) unless find_connection(tagged_user).nil?
         end
-
         sockets_to_be_notified.each { |s| s.send('han cargado un nuevo documento!') }
-
     end
   end
 
