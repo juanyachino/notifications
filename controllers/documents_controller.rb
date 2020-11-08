c217487218b6cbdaec9beffe02870730e6e26962
leonardoRd <gaitanleonardo6@gmail.com>
2020-11-08 19:06:47 -0300

    [finishes #175624788] excepciones agregadas en documents controllers y services

--- controllers/documents_controller.rb
+++ controllers/documents_controller.rb
@@ -18,26 +18,39 @@ class DocumentsController < Sinatra::Base
   attr_accessor :users, :documents, :is_admin
 
   get '/documents' do
-    hash = DocumentServices.load_all(session[:user_id])
+    begin
+      hash = DocumentServices.load_all(session[:user_id])
+    rescue ArgumentError => e
+      return erb :admin, layout: :layoutlogin, locals: { errorMessage: e.message }
+    end
     if hash[:error].nil?
       @is_admin = true
       @documents = hash[:documents]
       @users = hash[:users]
       erb :upload, layout: :layoutlogin
-    else
-      @error = hash[:error]
-      erb :admin, layout: :layoutlogin
     end
   end
 
   get '/userdocs' do
     self.documents = User.find_by_id(session[:user_id]).documents
+    @is_admin = if UserServices.admin?(session[:user_id])
+                  true
+                else
+                  false
+                end
     erb :userdocs, layout: :layoutlogin
   end
+
   get '/publicdocs' do
     self.documents = Document.all
+    @is_admin = if UserServices.admin?(session[:user_id])
+                  true
+                else
+                  false
+                end
     erb :publicdocs, layout: :layoutlogin
   end
+
   get '/showdocument' do
     erb :show_file, layout: :layoutlogin
   end
@@ -48,10 +61,17 @@ class DocumentsController < Sinatra::Base
              date: params['date'],
              uploader: User.find_by_id(session[:user_id]).username,
              subject: params['subject'] }
-
+    begin
     doc = DocumentServices.create_file(OpenStruct.new(hash))
+    rescue ArgumentError => e
+      return erb :error, layout: :layoutlogin, locals: { errorMessage: e.message }
+  end
     if !doc.nil?
-      UserServices.send_notifications(DocumentServices.tag_users(doc, params['tagged']))
+      begin
+          UserServices.send_notifications(DocumentServices.tag_users(doc, params['tagged']))
+      rescue ArgumentError => e
+        return erb :error, layout: :layoutlogin, locals: { errorMessage: e.message }
+        end
       redirect '/documents'
     else
       [500, {}, 'Internal Server Error']
